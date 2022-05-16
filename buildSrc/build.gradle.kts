031095bf5eedd0522025494f1a96767959455dda
Shalom Mathews <shalommathews05@gmail.com>
2022-05-16 14:02:07 -0400

    Initial commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id ("org.gradle.kotlin.kotlin-dsl") version "2.1.6"
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
---
+++ buildSrc/src/main/kotlin/GenerateConfigPlugin.kt
@@ -0,0 +1,8 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GenerateConfigPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}
